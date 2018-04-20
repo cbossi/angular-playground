535d9fc9b3d0fa4b015fe0f58172f4a4dca5595f
Christian Bosshard <bosshardchr@gmail.com>
2018-04-20 12:52:36 +0200

    Fix title-bug on home component.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,18 @@
 import {Component} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {RoutedComponent} from '../shared/routed.component';
+import {TitleService} from '../title/title.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent extends RoutedComponent {
+
+  constructor(activatedRoute: ActivatedRoute,
+              titleService: TitleService) {
+    super(activatedRoute, titleService);
+  }
 }
+
